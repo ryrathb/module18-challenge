ba97a948607d91915ccb1a205218ed3d48d42939
Ryan Rathbun <ryrathb@arizona.edu>
2022-08-30 15:12:16 -0700

    push

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -3,16 +3,18 @@ const { User, Thought } = require('../models');
 const userController = {
     getAllUsers(req, res) {
         User.find({})
+            .populate({ path: 'thoughts', select: '-__v' })
+            .populate({ path: 'friends', select: '-__v' })
+            .select('-__v')
             .then(userData => res.json(userData))
             .catch(error => {
-                console.log(error);
                 res.status(400).json(error);
             });
     },
     getUserByID({ params }, res) {
         User.findOne({ _id: params.id })
-            .populate('thoughts')
-            .populate('friends')
+            .populate({ path: 'friends', select: '-__v' })
+            .populate({ path: 'thoughts', select: '-__v', populate: { path: 'reactions' } })
             .select('-__v')
             .then(userData => {
                 if (!userData) {
@@ -22,12 +24,11 @@ const userController = {
                 res.json(userData);
             })
             .catch(error => {
-                console.log(error);
                 res.status(400).json(error);
             });
     },
-    createUser({ params }, res) {
-        User.findOneAndUpdate({ _id: params.id }, body, { new: true, runValidators: true })
+    createUser({ body }, res) {
+        User.create({ username: body.username, email: body.email })
             .then(userData => {
                 if(!userData) {
                     res.status(404).json({ message: 'No user found with that specific ID :(' });
@@ -63,7 +64,7 @@ const userController = {
             .catch(error => res.status(400).json(error));
     },
     addFriend({ params }, res) {
-        User.findOneAndUpdate({ _id: params.id }, { $addToSet: { friends: params.friendID } }, { runValidators: true })
+        User.findOneAndUpdate({ _id: params.userId }, { $push: { friends: params.friendId } }, { new: true, runValidators: true })
             .then(userData => {
                 if (!userData) {
                     res.status(404).json({ message: 'No user found with that specific ID :(' });
@@ -74,7 +75,7 @@ const userController = {
             .catch(error => res.status(400).json(error));
     },
     removeFriend({ params }, res) {
-        User.findOneAndUpdate({ _id: params.id }, { $pull: {friends: params.friendID } }, { runValidators: true })
+        User.findOneAndUpdate({ _id: params.userId }, { $pull: {friends: params.friendID } })
             .then(userData => {
                 if (!userData) {
                     res.status(404).json({ message: 'No user found with this id!' });
